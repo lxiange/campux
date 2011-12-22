2dcf21b4a70d31eea23c67b7ecc525a858d03692
yuyang@bizdata-info.com <yuyang@bizdata-info.com@f38e9f7a-1d5a-cee3-38f9-bb5e58037a57>
2011-12-22 18:22:38 +0000

    Update servers. dist/lib/intellix.jar为闭源商业程序，不开放源码。

--- Servers/src/com/bizdata/campux/server/Main.java
+++ Servers/src/com/bizdata/campux/server/Main.java
@@ -1,6 +1,7 @@
 package com.bizdata.campux.server;
 
 import com.bizdata.campux.server.userstatus.ServerUserStatus;
+import com.bizdata.campux.server.wifilocator.ServerWifiLocator;
 import java.io.FileInputStream;
 
 /**
@@ -10,8 +11,20 @@ import java.io.FileInputStream;
 public class Main {
     static public void main(String args[]) throws Exception{
         com.bizdata.campux.sdk.Config.init(new FileInputStream("sdk.config"));
-        System.out.println("startServer");
-        ServerUserStatus instance = new ServerUserStatus();
+        if(args.length==0){
+            System.out.println("Please choose service to start:");
+            System.out.println("    UserStatus");
+            System.out.println("    Locator");
+            return;
+        }
+        
+        ServerBase instance = null;
+        if( "userstatus".equalsIgnoreCase(args[0])){
+            instance = new ServerUserStatus();
+        }else if( "locator".equalsIgnoreCase(args[0])){
+            instance = new ServerWifiLocator();
+        }
+        System.out.println("start server: " + instance.getName());
         instance.startServer();
     }
 }
