a0c7f25e5511ced766595d621c65a98dff870458
gaolei@bizdata-info.com <gaolei@bizdata-info.com@f38e9f7a-1d5a-cee3-38f9-bb5e58037a57>
2011-12-28 08:32:18 +0000

    修正 UserInfoSAX和User中的bug

--- SDK/src/com/bizdata/campux/sdk/saxhandler/UserInfoSAX.java
+++ SDK/src/com/bizdata/campux/sdk/saxhandler/UserInfoSAX.java
@@ -19,29 +19,63 @@ package com.bizdata.campux.sdk.saxhandler;
 import java.io.InputStream;
 import java.util.LinkedList;
 import org.xml.sax.Attributes;
-
+import com.bizdata.campux.sdk.*;
 /**
  * SAX handler
  * @author yuy
  */
 public class UserInfoSAX extends SAXHandlerBase{
 	
-    protected LinkedList<String> m_vars = new LinkedList<String>();
     protected String m_responseStr = null;
+    protected LinkedList<Publisher> m_pubs=new LinkedList<Publisher>();
+    protected LinkedList<InfoMessage> m_msgs=new LinkedList<InfoMessage>();
+    protected LinkedList<InfoMsgIndex> m_indices=new LinkedList<InfoMsgIndex>();
+    protected Publisher pub=null;
+    protected InfoMessage msg=null;
+    protected InfoMsgIndex index=null;
+    protected InfoEvent event=null;
     /**
-     * get names of the variables in the system
+     * get infomessages
+     * @return 
+     */
+    public InfoMessage[] getInfoMessages(){
+    	InfoMessage[] msgs = new InfoMessage[m_msgs.size()];
+        for(int i=0; i<msgs.length; i++){
+            msgs[i] = m_msgs.get(i);
+        }
+        return msgs;
+    }
+    /**
+    * get publishers
+    * @return 
+    */
+    public Publisher[] getPublishers(){
+	   Publisher[] pubs = new Publisher[m_pubs.size()];
+       for(int i=0; i<pubs.length; i++){
+    	   pubs[i] = m_pubs.get(i);
+       }
+       return pubs;
+    }
+    /**
+     * get message indices
+     * @return 
+     */
+     public InfoMsgIndex[] getMessageIndices(){
+    	InfoMsgIndex[] indices = new InfoMsgIndex[m_indices.size()];
+        for(int i=0; i<indices.length; i++){
+        	indices[i] = m_indices.get(i);
+        }
+        return indices;
+     }
+    
+   /**
+     * get value of response with singleton string
      * @return 
      */
     public String getResponseString(){
         return m_responseStr;
     }
-    public String[] getList(){
-        String[] vs = new String[m_vars.size()];
-        for(int i=0; i<vs.length; i++){
-            vs[i] = m_vars.get(i);
-        }
-        return vs;
-    }
+   
     /**
      * Deals with the XML content via SAX
      * @param content
@@ -52,23 +86,48 @@ public class UserInfoSAX extends SAXHandlerBase{
     protected void contentReceived(String content, String tagname, Attributes m_tagattr){
     	System.out.println("contentrecevied:"+tagname);
         if("ok".equalsIgnoreCase(tagname)){
-            m_responseStr = content;
+            m_responseStr = content.trim();
         }else if( "a".equalsIgnoreCase(tagname) ){
-            m_vars.add(content.trim());
+            pub=new Publisher();
+            pub.m_user=m_tagattr.getValue("n");
+        }else if( "ip".equalsIgnoreCase(tagname) ){
+        	pub.p_iconname=content.trim();
+        }else if( "dp".equalsIgnoreCase(tagname) ){
+        	pub.p_displayname=content.trim();
+        }else if( "cp".equalsIgnoreCase(tagname) ){
+        	pub.p_infotype.add(content.trim());
+        	while("cp".equalsIgnoreCase(tagname) )
+        		pub.p_infotype.add(content.trim());
+        	m_pubs.add(pub);
         }else if( "i".equalsIgnoreCase(tagname) ){
-            m_vars.add(content);
+            index=new InfoMsgIndex();
+            index.i_preview=content.trim();
+            index.i_publisher=m_tagattr.getValue("xp");
+            index.i_pubcategory=m_tagattr.getValue("xc");
+            try{
+                index.i_id = Integer.parseInt(m_tagattr.getValue("xd"));
+            }catch(Exception exc){
+            	index.i_id = -1;
+            }
+            m_indices.add(index);
         }else if( "t".equalsIgnoreCase(tagname) ){
-            m_vars.add(content);            
+        	msg=new InfoMessage();
+        	msg.m_title=content.trim();
         }else if( "d".equalsIgnoreCase(tagname) ){
-            m_vars.add(content);            
-        }else if( "u".equalsIgnoreCase(tagname) ){
-            m_vars.add(content);            
+        	msg.m_date=content.trim();
+        }else if( "l".equalsIgnoreCase(tagname) ){
+        	msg.m_link=content.trim();
         }else if( "co".equalsIgnoreCase(tagname) ){
-            m_vars.add(content);            
+        	msg.m_content=content.trim();
         }else if( "e".equalsIgnoreCase(tagname) ){
-            m_vars.add(content);            
-        }else if( "c".equalsIgnoreCase(tagname) ){
-            m_vars.add(content);            
+        	event= new InfoEvent();
+        }else if( "et".equalsIgnoreCase(tagname) ){
+        	event.event_title=content.trim();
+        }else if( "ed".equalsIgnoreCase(tagname) ){
+        	event.event_date=content.trim();
+        }else if( "ea".equalsIgnoreCase(tagname) ){
+        	event.event_place=content.trim();
+        	msg.events.add(event);
         }
     }
     
@@ -87,12 +146,12 @@ public class UserInfoSAX extends SAXHandlerBase{
         str.append("<rp s=\"");
         str.append("sessionID\" n=\"");
         str.append(p_name+"\">");
-        str.append("<i b64=\"true\">"+icon+"</i>");
-        str.append("<d>"+p_displayName+"</d>");
+        str.append("<ip b64=\"true\">"+icon+"</ip>");
+        str.append("<dp>"+p_displayName+"</dp>");
         for(String kind : p_infoKind){
-        	str.append("<c>");
+        	str.append("<cp>");
             str.append(kind);
-            str.append("</c>");        
+            str.append("</cp>");        
         }
         str.append("<rp>");
         return str.toString();
@@ -137,20 +196,33 @@ public class UserInfoSAX extends SAXHandlerBase{
     }
 
     public String prepareInfoPublish(String sessionID,String i_title,String i_date,
-    		String i_link,String i_content,String i_kind,String a_name,String a_date,String a_address)
+    		String i_link,String i_content,String i_publisher,String i_kind,InfoEvent[] ee,String[] uu,String[] gg)
     {
     	StringBuilder str = new StringBuilder();
         str.append("<si s=\""+sessionID+"\">");
         str.append("<t b64=\"true\">"+i_title+"</t>");
         str.append("<d>"+i_date+"</d>");
-        str.append("<u>"+i_link+"</u>");
+        str.append("<l>"+i_link+"</l>");
         str.append("<co b64=\"true\">"+i_content+"</co>");
+        str.append("<p>"+i_publisher+"</p>");
         str.append("<c>"+i_kind+"</c>");
-        str.append("<e>");
-        str.append("<t b64=\"true\">"+a_name+"</t>");
-        str.append("<d>"+a_date+"</d>");
-        str.append("<t b64=\"true\">"+a_address+"</t>");
-        str.append("</e>");
+        for(int i=0;i<ee.length;i++){        	
+        	str.append("<e>");
+            str.append("<et b64=\"true\">"+ee[i].event_title+"</et>");
+            str.append("<ed>"+ee[i].event_date+"</ed>");
+            str.append("<ea b64=\"true\">"+ee[i].event_place+"</ea>");
+            str.append("</e>");
+        }
+        for(String user : uu){
+            str.append("<u>");
+            str.append(user);
+            str.append("</u>");
+        }
+        for(String group : gg){
+            str.append("<g>");
+            str.append(group);
+            str.append("</g>");
+        }        
         str.append("</si>");
         return str.toString();
     }
@@ -165,21 +237,6 @@ public class UserInfoSAX extends SAXHandlerBase{
         return str.toString();
     }
 
-    
- /*   public static void main(String[] args){
-    	UserInfoSAX ui=new UserInfoSAX();
-    	String sessionID="1234556677";
-    	String infoID="INFOA";
-    	String[] pt={"xx","yy","zz"};
-    	System.out.println(ui.preparePublisherList());
-    	System.out.println(ui.prepareAccountInitialization(sessionID));
-    	System.out.println(ui.prepareUpdateCheck(sessionID));
-    	System.out.println(ui.prepareInfoAchieve(sessionID,infoID));
-    	System.out.println(ui.prepareInfoDelete(sessionID,infoID));
-    	System.out.println(ui.preparePublisherRegistration(sessionID,"a","b","c",pt));
-    	System.out.println(ui.prepareInfoPublish(sessionID,"a","b","c","d","e","f","g","h"));
-    }
-*/
 }
 
 
