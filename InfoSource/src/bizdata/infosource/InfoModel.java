b87f881df64319899aa8120c350bf3ad83087d13
yuyang@bizdata-info.com <yuyang@bizdata-info.com@f38e9f7a-1d5a-cee3-38f9-bb5e58037a57>
2012-03-27 14:06:45 +0000

    InfoSource爬取百合信息，使用了HTMLUNIT开源项目，协议兼容

---
+++ InfoSource/src/bizdata/infosource/InfoModel.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bizdata.infosource;
+
+import com.bizdata.intellix.docanalysis.Model;
+
+/**
+ *
+ * @author yuy
+ * @date 2012-03-11 04:44:25
+ */
+public class InfoModel {
+    public String m_user, m_password, m_path;
+    public Model m_model;
+}
